947cbd107aeca5ec8279728acdcb132e9a521016
toni07 <ronchman07@yahoo.fr>
2015-06-03 15:26:25 +0200

    Update StdTableFilter.js

--- js/StdTableFilter.js
+++ js/StdTableFilter.js
@@ -1,44 +1,72 @@
+"use strict";
 var StdTableFilter = function(options){
 
-	var me = this;
-	/////	span filter		/////
-	var spanAddFilter = $('<span class="cubee-action">+</span>');
-	spanAddFilter.on('click', function(){
-		var thisElem = $(this);
-		console.log('##thisElem', me.spanAddFilter.nbFilterElem);
-		if(null == me.spanAddFilter.nbFilterElem){
-			me.spanAddFilter.nbFilterElem = 0;
-		}
-		if(0 == me.spanAddFilter.nbFilterElem){
-			me.boutonSendFilter.show();
-		}
-		me.spanAddFilter.nbFilterElem = me.spanAddFilter.nbFilterElem + 1;
-		var parent = thisElem.parents('div.cubee-table-filter');
-		var divModel = parent.children('div.cubee-model');
-		Http.copyModel(divModel, parent, function(newDiv){
-			var spanDeleteLine = $('<span class="cubee-action"> X </span>');
-			spanDeleteLine.on('click', function(){
-				newDiv.remove();
-				me.spanAddFilter.nbFilterElem = me.spanAddFilter.nbFilterElem - 1;
-				if(0 == me.spanAddFilter.nbFilterElem){
-					me.boutonSendFilter.hide();
-				}
-			});
-			newDiv.append(spanDeleteLine);
-		});
-	});
-	this.spanAddFilter = spanAddFilter;
+	var me = this;	
 	
-	/////	<select> for filter field	/////
-	var selectField = $('<select disabled="disabled"><option>** choix **</option></select>');
-	for(var i=0; i<options.columnList.length; i++){
-		var column = options.columnList[i];
-		selectField.append($('<option value="'+ column.fieldId +'">'+ column.label +'</option>'));
+	/////////// global div for filters /////////////
+	this.createGlobalDiv = function(){
+	
+		me.globalDiv = $('<div class="cubee-table-filter"><div>'
+				+	'	<div class="cubee-table-filter-title">Filtres </div>'
+				+	'	<div class="cubee-model">'
+				+	'		<select disabled="disabled"><option>** choix **</option><option>=</option><option><=</option><option>>=</option><option>!=</option></select> <input disabled="disabled" name="filter-value" type="text" placeholder=" valeur" />'
+				+	'	</div>'
+				+ 	'</div></div>');
 	}
-	this.selectField = selectField;
 	
-	/////	bouton post filter	/////
-	var boutonSendFilter = $('<input type="submit" />');
-	boutonSendFilter.hide();
-	this.boutonSendFilter = boutonSendFilter;
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////// constructor body ///////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	this.constructor = function(){
+	
+		/////	span filter		/////
+		var spanAddFilter = $('<span class="cubee-action">+</span>');
+		spanAddFilter.on('click', function(){
+			var thisElem = $(this);
+			console.log('##thisElem', me.spanAddFilter.nbFilterElem);
+			if(null == me.spanAddFilter.nbFilterElem){
+				me.spanAddFilter.nbFilterElem = 0;
+			}
+			if(0 == me.spanAddFilter.nbFilterElem){
+				me.boutonSendFilter.show();
+			}
+			me.spanAddFilter.nbFilterElem = me.spanAddFilter.nbFilterElem + 1;
+			var parent = thisElem.parent().parent();
+			var divModel = parent.children('div.cubee-model');
+			Http.copyModel(divModel, parent, function(newDiv){
+				var spanDeleteLine = $('<span class="cubee-action"> X </span>');
+				spanDeleteLine.on('click', function(){
+					newDiv.remove();
+					me.spanAddFilter.nbFilterElem = me.spanAddFilter.nbFilterElem - 1;
+					if(0 == me.spanAddFilter.nbFilterElem){
+						me.boutonSendFilter.hide();
+					}
+				});
+				newDiv.append(spanDeleteLine);
+			});
+		});
+		this.spanAddFilter = spanAddFilter;
+		
+		/////	<select> for filter field	/////
+		var selectField = $('<select disabled="disabled"><option>** choix **</option></select>');
+		for(var i=0; i<options.columnList.length; i++){
+			var column = options.columnList[i];
+			selectField.append($('<option value="'+ column.fieldId +'">'+ column.label +'</option>'));
+		}
+		me.selectField = selectField;
+		
+		/////	bouton post filter	/////
+		var boutonSendFilter = $('<input type="submit" />');
+		boutonSendFilter.hide();
+		me.boutonSendFilter = boutonSendFilter;
+		me.createGlobalDiv();
+		
+		me.globalDiv.find('.cubee-table-filter-title').append(me.spanAddFilter);
+		me.globalDiv.find('.cubee-model').prepend(me.selectField);
+		me.globalDiv.append(me.boutonSendFilter);
+	}
+	
+	this.constructor();
+	
+	
 };
