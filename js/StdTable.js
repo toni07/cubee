bb7c703b127db38be93acf82921cdeed090fc8d5
toni07 <ronchman07@yahoo.fr>
2015-06-04 14:17:19 +0200

    Update StdTable.js

--- js/StdTable.js
+++ js/StdTable.js
@@ -82,6 +82,11 @@ var StdTable = function(idDiv, options){
 		me.htmlTable = $(me.tableElem).jStdTable('http://opendata.paris.fr/api/records/1.0/search?dataset=troncon_voie', htmlColumnList, this.stdTableFilter);*/
 	}
 	
+	this.triggerPageChange = function(pageNumber){
+	
+		me.getData(pageNumber);
+	};
+	
 	this.createFilter = function(p1){
 		console.log('TODO: create filters with param: ' + p1);
 	}
@@ -94,50 +99,48 @@ var StdTable = function(idDiv, options){
 	/**
 	 * fires the HttpRequest to get the data from the server 
 	*/
-	this.getData = function(){
+	this.getData = function(pageNumber){
 		
 		var paramsIni = {
-			'page-num': thisTable.currentPage
+			'page-num': pageNumber
 		};
-		Http.sendRequest(urlGet, paramsIni, function(response){
-			var jsonResult = response[jsonResultKey];
-			var tBodyElem = thisTable.find('tbody').eq(0);
-			tBodyElem.html('');
-			for(var i=0; i<jsonResult.length; i++){
-				var trElem = document.createElement('tr');
-				console.log(jsonResult[i]);
-				if(null != options.onClickRow){
-					var $trElem = $(trElem);
-					(function(jsonData){
-						$trElem.addClass('cubee-action');
-						$trElem.on('click', function(){
-							options.onClickRow(jsonData);
-						});
-					})(jsonResult[i]);
-				}		
-				for(var j=0; j<options.columnList.length; j++){
-					var tmpColumn = options.columnList[j];
-					if(null == tmpColumn.visible || tmpColumn.visible){
-						var tdElem = document.createElement('td');
-						$(tdElem).html(tmpColumn.label);
-						trElem.appendChild(tdElem);
-					}
-				}
-				tBodyElem.append(trElem);
-			}
+		Http.sendRequest(me.urlData, paramsIni, function(response){
+			var jsonResult = response[me.jsonKeyData];
+			me.setData(jsonResult);
+			me.populateRow();
 		});
-	}
+	};
+	
+	this.setData = function(jsonData){
+	
+		me.jsonData = jsonData;
+		/*me.jsonData = [
+			{
+				toto1: 'yooo',
+				toto2: 'gahah',
+				toto3: 12,
+				toto4: '2012-05-06',
+				toto5: 222
+			},
+			{
+				toto1: 'fzedz',
+				toto2: 'cccccc',
+				toto3: 48,
+				toto4: '2017-05-06',
+				toto5: 1
+			}
+		];*/
+	};
 
 	/**
 	 * creates the rows that display data
 	*/
 	this.populateRow = function(){
 		
-		var tBodyElem = me.htmlTable.find('tbody').eq(0);
+		var tBodyElem = me.tableElem.find('tbody').eq(0);
 		tBodyElem.html('');
 		for(var i=0; i<me.jsonData.length; i++){
 			var trElem = document.createElement('tr');
-			console.log(me.jsonData[i]);
 			if(null != options.onClickRow){
 				var $trElem = $(trElem);
 				(function(jsonRow){
@@ -147,13 +150,11 @@ var StdTable = function(idDiv, options){
 					});
 				})(me.jsonData[i]);
 			}		
-			for(var j=0; j<options.columnList.length; j++){
-				var tmpColumn = options.columnList[j];
-				if(null == tmpColumn.visible || tmpColumn.visible){
-					var tdElem = document.createElement('td');
-					$(tdElem).html(tmpColumn.label);
-					trElem.appendChild(tdElem);
-				}
+			for(var j=0; j<me.visibleColumnList.length; j++){
+				var tmpColumn = me.visibleColumnList[j];
+				var tdElem = document.createElement('td');
+				$(tdElem).html(me.jsonData[i][tmpColumn.fieldKey]);
+				trElem.appendChild(tdElem);				
 			}
 			tBodyElem.append(trElem);
 		}
@@ -164,34 +165,58 @@ var StdTable = function(idDiv, options){
 	//////////////////////////////////////////////////////////////////////////////////////////////
 	var me = this;
 	this.constructor = function(){
+	
+		me.columnList = options.columnList;
+		me.visibleColumnList = new Array();
+		for(var i=0; i<me.columnList.length; i++){
+			var tmpColumn = me.columnList[i];
+			if(null == tmpColumn.visible || tmpColumn.visible){
+				me.visibleColumnList.push(tmpColumn);
+			}
+		}
+		
 		var tableElem = document.createElement('table');
 		tableElem.className = 'cubee';
 		var trElem = document.createElement('tr');
 		var htmlColumnList = new Array();
 		var nbColumnsVisible = 0;
 		var columnSelectHeader = $('<select></select>');
-		for(var i=0; i<options.columnList.length; i++){
-			var tmpColumn = options.columnList[i];
+		for(var i=0; i<me.columnList.length; i++){
+			var tmpColumn = me.columnList[i];
 			columnSelectHeader.append($('<option value="'+ tmpColumn.fieldId +'">'+ tmpColumn.label +'</option>'));
 		}
 		
-		for(var i=0; i<options.columnList.length; i++){
-			var tmpColumn = options.columnList[i];
-			if(null == tmpColumn.visible || tmpColumn.visible){
-				nbColumnsVisible = nbColumnsVisible + 1;
-				var colElem = document.createElement('col');
-				$(colElem).attr('style', 'width:'+ tmpColumn.width +'');
-				tableElem.appendChild(colElem);
-				var thElem = document.createElement('th');
-				var $thElem = $(thElem);
-				$thElem.append(columnSelectHeader.clone().val(tmpColumn.fieldId));
-				var $a = $('<a href="#">&squf;</a>');
-				$a.orderby = 0;
-				$a.fieldId = i;
-				htmlColumnList.push($a);
-				$thElem.append($a);
-				trElem.appendChild(thElem);
-			}		
+		for(var i=0; i<me.visibleColumnList.length; i++){
+			var tmpColumn = me.visibleColumnList[i];		
+			nbColumnsVisible = nbColumnsVisible + 1;
+			var colElem = document.createElement('col');
+			$(colElem).attr('style', 'width:'+ tmpColumn.width +'');
+			tableElem.appendChild(colElem);
+			var thElem = document.createElement('th');
+			var $thElem = $(thElem);
+			var selectElemClone = columnSelectHeader.clone();
+			(function(column, index){
+				selectElemClone.on('change', function(){
+					var selectedFieldId = $(this).val();
+					var newVisbileColumn = null;
+					for(var i=0; i<me.columnList.length; i++){
+						var tmpCol = me.columnList[i];
+						if(selectedFieldId == tmpCol.fieldId){
+							newVisbileColumn = tmpCol;
+						}
+					}
+					me.visibleColumnList[index] = newVisbileColumn;
+					me.populateRow();
+				});
+			})(tmpColumn, i);
+			$thElem.append(selectElemClone.val(tmpColumn.fieldId));
+			var $a = $('<a href="#">&squf;</a>');
+			$a.orderby = 0;
+			$a.fieldId = i;
+			htmlColumnList.push($a);
+			$thElem.append($a);
+			trElem.appendChild(thElem);
+					
 		}
 		tableElem.appendChild(trElem);
 		//---------td 'no row'----------------//
@@ -208,7 +233,7 @@ var StdTable = function(idDiv, options){
 		var divElem = document.getElementById(idDiv);
 		divElem.appendChild(tableElem);
 		me.stdTableFilter = new StdTableFilter(options);
-		me.stdTablePaging = new StdTablePaging(options);
+		me.stdTablePaging = new StdTablePaging(me, options);
 		me.tableElem = $(tableElem);
 		me.globalFormElem = $('<form></form>');
 		me.globalFormElem.on('submit', function(e){
@@ -216,22 +241,8 @@ var StdTable = function(idDiv, options){
 			alert('todo: submit');
 			return false;
 		});
-		me.jsonData = [
-			{
-				toto1: 'yooo',
-				toto2: 'gahah',
-				toto3: 12,
-				toto4: '2012-05-06',
-				toto5: 222
-			},
-			{
-				toto1: 'fzedz',
-				toto2: 'cccccc',
-				toto3: 48,
-				toto4: '2017-05-06',
-				toto5: 1
-			}
-		];
+		me.urlData = 'http://opendata.paris.fr/api/records/1.0/search?dataset=troncon_voie';
+		me.jsonKeyData = 'records';
 		me.createHtmlTable();
 	}
 	
