5759c60fae533da0227f47416e38b9efc37b5cfa
toni07 <ronchman07@yahoo.fr>
2015-06-10 16:57:25 +0200

    Create ServletExample.java

---
+++ server_examples/ServletExample.java/attributes-private-static-final-long-serialVersionUID-1565785544L-private-static-Logger-LOGGER-Logger.getLogger-PeseeServlet.class-private-static-final-String-HTTP_INPUT_ID_SORTIE-id-sortie-private-static-final-List-DBField-listGUIField-new-ArrayList-DBField-private-static-final-List-String-operatorList-new-ArrayList-String-static-listGUIField.add-new-DBField-0-num_pesee-p.num_pesee-N-pes-e-true-true-listGUIField.add-new-DBField-1-date_ent_pesee-DATE_FORMAT-p.date_ent_pesee-d/m/ServletExample.java
@@ -0,0 +1,29 @@
+// attributes
+	private static final long serialVersionUID = 1565785544L;
+	private static Logger LOGGER = Logger.getLogger(PeseeServlet.class);
+	private static final String HTTP_INPUT_ID_SORTIE = "id-sortie";
+	private static final List<DBField> listGUIField = new ArrayList<DBField>();
+	private static final List<String> operatorList = new ArrayList<String>();
+
+	static {
+		listGUIField.add(new DBField(0, "num_pesee", "p.num_pesee", "N° pesée", true, true));
+		listGUIField.add(new DBField(1, "date_ent_pesee", "DATE_FORMAT(p.date_ent_pesee, '%d/%m/%Y')", "Date entrée", true, true));
+		listGUIField.add(new DBField(2, "num_park_camion", "c.num_park_camion", "Num park", true, true));
+		listGUIField.add(new DBField(3, "ptac_camion", "c.ptac_camion", "Ptac", true, true));
+		listGUIField.add(new DBField(4, "charge_utile_camion", "c.charge_utile_camion", "Charge utile", true, true));
+		listGUIField.add(new DBField(5, "nom_data_provider", "dp.nom_data_provider", "Fournisseur", true, true));
+		listGUIField.add(new DBField(6, "poids_net_pesee", "p.poids_net_pesee", "Poids net", true, true));
+		listGUIField.add(new DBField(7, "num_tour_code_pesee", "p.num_tour_code_pesee", "N° tour", true, true));
+		listGUIField.add(new DBField(8, "taux_remp_reel_pesee", "p.taux_remp_reel_pesee", "Tx remp. réel", true, true));
+		listGUIField.add(new DBField(9, "taux_remp_stat_pesee", "p.taux_remp_stat_pesee", "Tx remp. stat", true, true));
+		listGUIField.add(new DBField(10, "nom_organisation", "orga.nom_organisation", "Organisation", true, true));
+		listGUIField.add(new DBField(11, "nom_type_dechet", "td.nom_type_dechet", "Type déchet", true, true));
+		listGUIField.add(new DBField(12, "list_error", "has_pesee_in_error.list_error", "Erreurs", true, true));
+		listGUIField.add(new DBField(13, "id_pesee", "p.id_pesee", "Id pesée", true, true));
+		listGUIField.add(new DBField(14, "id_camion", "c.id_camion", "Id camion", true, true));
+
+		operatorList.add(" = ");
+		operatorList.add(" >= ");
+		operatorList.add(" <= ");
+		operatorList.add(" LIKE ");
+	}
