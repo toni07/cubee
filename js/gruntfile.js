38c0acd94b4ebdefbd1b001cbba824e24e51b45f
toni07 <ronchman07@yahoo.fr>
2015-07-09 15:25:43 +0200

    Update gruntfile.js

--- js/gruntfile.js
+++ js/gruntfile.js
@@ -2,34 +2,58 @@ module.exports = function (grunt) {
     grunt.initConfig({
 
     paths: {
-            src: {
-                js: 'cubee/*.js'
-            },
-            dest: {
-                js: 'bin/cubee.js',
-                jsMin: 'bin/cubee.min.js'
-            }
-        },
-        concat: {
-            js: {
-                options: {
-                    separator: ';'
-                },
-                src: '<%= paths.src.js %>',
-                dest: '<%= paths.dest.js %>'
-            }
-        },
-        uglify: {
-            options: {
-                compress: true,
-                mangle: true,
-                sourceMap: true
-            },
-            target: {
-                src: '<%= paths.src.js %>',
-                dest: '<%= paths.dest.jsMin %>'
-            }
-        }
+		src: {
+			js: 'cubee/*.js',
+			concatjs: 'bin/modules/*.js'
+		},
+		dest: {
+			js: 'bin/cubee.table.js',
+			jsMin: 'bin/cubee.min.js'
+		}
+	},
+	concat: {
+		js: {
+			options: {
+				separator: ';'
+			},
+			src: '<%= paths.src.js %>',
+			dest: '<%= paths.dest.js %>'
+		}
+	},
+	uglify: {
+		options: {
+			compress: true,
+			mangle: true,
+			sourceMap: true
+		},
+		target: {
+			src: '<%= paths.src.concatjs %>',
+			dest: '<%= paths.dest.jsMin %>'
+		}
+	}
+});
+
+grunt.registerTask("prepareModules", "Finds and prepares modules for concatenation.", function() {
+
+    // get all module directories
+    grunt.file.expand("cubee/*").forEach(function (dir) {
+
+        // get the module name from the directory name
+        var dirName = dir.substr(dir.lastIndexOf('/')+1);
+
+        // get the current concat object from initConfig
+        var concat = grunt.config.get('concat') || {};
+
+        // create a subtask for each module, find all src files
+        // and combine into a single js file per module
+        concat[dirName] = {
+            src: [dir + '/**/*.js'],
+            dest: 'bin/modules/' + dirName + '.min.js'
+        };
+
+        // add module subtasks to the concat task in initConfig
+        grunt.config.set('concat', concat);
+    });
 });
 
 // load plugins
@@ -37,7 +61,7 @@ grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.loadNpmTasks('grunt-contrib-uglify');
 
 // register at least this one task
-grunt.registerTask('default', [ 'concat', 'uglify' ]);
+grunt.registerTask('default', [ 'prepareModules', 'concat', 'uglify' ]);
 
 
 };
