794c7e299fe6b316594e625f36a65587b767affe
toni <epardeau.antoine@gmail.com>
2015-07-25 12:26:18 +0200

    added pie chart

---
+++ js/cubee/chart/CubeeChartPie.js
@@ -0,0 +1,38 @@
+/**
+ * Class used to draw a Pie Chart
+ * @author toni07 25/07/2015
+*/
+"use strict";
+var CubeeChartPie = function(divElem, data, options){
+
+	var me = this;
+		
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////// constructor body ///////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	this.construct = function(){
+	
+		var canvas = divElem;
+		var ctx = canvas.getContext("2d");
+		var lastend = 0;
+		var myTotal = 0; // Automatically calculated so don't touch
+
+		for (var e = 0; e < data.length; e++) {
+		  myTotal += data[e].value;
+		}
+
+		for (var i = 0; i < data.length; i++) {
+		  ctx.fillStyle = data[i].color;
+		  ctx.beginPath();
+		  ctx.moveTo(canvas.width / 2, canvas.height / 2);
+		  // Arc Parameters: x, y, radius, startingAngle (radians), endingAngle (radians), antiClockwise (boolean)
+		  ctx.arc(canvas.width / 2, canvas.height / 2, canvas.height / 2, lastend, lastend + (Math.PI * 2 * (data[i].value / myTotal)), false);
+		  ctx.lineTo(canvas.width / 2, canvas.height / 2);
+		  ctx.fill();
+		  lastend += Math.PI * 2 * (data[i].value / myTotal);
+		}
+	};
+	
+	this.construct();
+	
+};
