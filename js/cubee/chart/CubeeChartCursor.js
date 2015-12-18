baba6e3fbc1b99c15452f54ab5ef841bf0b24bb8
toni07 <ronchman07@yahoo.fr>
2015-12-18 15:10:07 +0100

    dynamic cursor chart

--- js/cubee/chart/CubeeChartCursor.js
+++ js/cubee/chart/CubeeChartCursor.js
@@ -8,10 +8,19 @@ Cubee.CubeeChartCursor = function(divElem, options){
 		
 		//dynamic parameters (can be changed) TODO: pass these in options
 		var svgHeight = 350;
-		var hasDataOutsideTheBox = false;
+		var valueList = {
+			min: 1000,
+			max: 8000,
+			value: 8600
+		};
+		var xRectLeftPosition = 90;
+		var rectWidth = 300;
 		
 		//calculated params (not to be changed!)
-		
+		var xRectRightPosition = xRectLeftPosition + rectWidth;
+		var xCursorPosition = xRectLeftPosition + (valueList.value * (rectWidth/(valueList.max - valueList.min)));
+		var xCursorLabelPosition = xCursorPosition - 25;
+		var hasDataOutsideTheBox = (valueList.value > valueList.max || valueList.value < valueList.min);
 		
 		var html = '<svg width="700" height="'+ svgHeight +'" style="border:1px solid #000000;">'
 			+ '			<defs>'
@@ -20,17 +29,28 @@ Cubee.CubeeChartCursor = function(divElem, options){
 			+ '					<stop offset="100%" style="stop-color:rgb(0,0,255);stop-opacity:1" />'
 			+ '				</linearGradient>'
 			+ '			</defs>'
-			+ '			<rect x="90" y="100" width="300" height="50" fill="url(#gradient)" />'
+			+ '			<rect x="'+ xRectLeftPosition +'" y="100" width="'+ rectWidth +'" height="50" fill="url(#gradient)" />'
 		if(hasDataOutsideTheBox){
-			html+=  '   <line x1="50" y1="125" x2="440" y2="125" style="stroke:rgb(50,50,50);stroke-width:1;" />'	//central line if data outside the box
+			var centerLineXStart = xRectLeftPosition;
+			if(valueList.value < valueList.min){
+				centerLineXStart = xCursorPosition;
+			}
+			var centerLineXEnd = xRectRightPosition;
+			if(valueList.value > valueList.max){
+				centerLineXEnd = xCursorPosition;
+			}
+			html+=  '   <line x1="'+ centerLineXStart +'" y1="125" x2="'+ centerLineXEnd +'" y2="125" style="stroke:rgb(50,50,50);stroke-width:1;" />'	//central line if data outside the box
 		}
 		
-		html+=  '   <line x1="90" y1="90" x2="90" y2="160" style="stroke:rgb(50,50,50);stroke-width:2;" />'	//left tick
-		html+=  '   <line x1="390" y1="90" x2="390" y2="160" style="stroke:rgb(50,50,50);stroke-width:2;" />'	//right tick
-		html+=  '   <line x1="200" y1="70" x2="200" y2="180" style="stroke:rgb(0,255,0);stroke-width:4;" />'	//cursor for the data
+		html+=  '   <line x1="'+ xRectLeftPosition +'" y1="90" x2="'+ xRectLeftPosition +'" y2="160" style="stroke:rgb(50,50,50);stroke-width:2;" />'	//left tick
+		html+=  '   <line x1="'+ xRectRightPosition +'" y1="90" x2="'+ xRectRightPosition +'" y2="160" style="stroke:rgb(50,50,50);stroke-width:2;" />'	//right tick
+		html+=  '   <line x1="'+ xCursorPosition +'" y1="70" x2="'+ xCursorPosition +'" y2="180" style="stroke:rgb(0,255,0);stroke-width:4;" />'	//cursor for the data
 		html+=  '   <text x="70" y="85" fill="#000000" font-family="calibri">Clim. max 10 ans</text>'	//label left
 		html+=  '   <text x="370" y="85" fill="#000000" font-family="calibri">Clim. min 10 ans</text>'	//label right
-		html+=  '   <text x="155" y="60" fill="#00ff00" font-family="calibri">Semaine dernière</text>'	//label data
+		html+=  '   <text x="'+ xCursorLabelPosition +'" y="60" fill="#00ff00" font-family="calibri" font-size="16" style="font-weight:bold;">Semaine dernière</text>'	//label data
+		html+=  '   <text x="70" y="176" fill="#000000" font-family="calibri" font-size="14">1000 kWh</text>'	//value left
+		html+=  '   <text x="370" y="176" fill="#000000" font-family="calibri" font-size="14">8000 kWh</text>'	//value right
+		html+=  '   <text x="'+ xCursorLabelPosition +'" y="200" fill="#00ff00" font-family="calibri" font-size="16" style="font-weight:bold;">'+ valueList.value +' kWh</text>'	//value data
 		html+= 
 			  '     </svg>'; 
 		divElem.innerHTML = html;
