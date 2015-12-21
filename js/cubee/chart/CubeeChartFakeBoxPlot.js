dea7f2d5c10bc14936b874411adc39a0da915aeb
toni07 <ronchman07@yahoo.fr>
2015-12-21 15:54:27 +0100

    modifs

--- js/cubee/chart/CubeeChartFakeBoxPlot.js
+++ js/cubee/chart/CubeeChartFakeBoxPlot.js
@@ -14,11 +14,15 @@ Cubee.CubeeChartFakeBoxPlot = function(divElem, options){
 			value: 7000
 		};
 		var xRectLeftPosition = 90;
+		var xPaddingLeft = 20;
+		var xValuePadding = 70;
 		var rectWidth = 300;
+		var nbTicks = 7;
 		
 		//calculated params (not to be changed!)
+		xRectLeftPosition = xRectLeftPosition + xPaddingLeft;
 		var xRectRightPosition = xRectLeftPosition + rectWidth;
-		var xCursorPosition = xRectLeftPosition + ((valueList.value - 1000)* (rectWidth/(valueList.max - valueList.min)));
+		var xCursorPosition = xRectLeftPosition + ((valueList.value - valueList.min)* (rectWidth/(valueList.max - valueList.min)));
 		var xCursorLabelPosition = xCursorPosition - 25;
 		var hasDataOutsideTheBox = (valueList.value > valueList.max || valueList.value < valueList.min);
 		
@@ -28,23 +32,31 @@ Cubee.CubeeChartFakeBoxPlot = function(divElem, options){
 			+ '			<rect x="'+ (xRectLeftPosition + 150) +'" y="100" width="'+ 125 +'" height="50" style="fill:rgb(255,0,0);" />'
 			+ '			<rect x="'+ (xRectLeftPosition + 250) +'" y="100" width="'+ 50 +'" height="50" style="fill:rgb(255,175,175);" />'
 		if(true || hasDataOutsideTheBox){
-			var centerLineXStart = xRectLeftPosition - 50;
+			var centerLineXStart = xRectLeftPosition - xValuePadding;
 			if(valueList.value < valueList.min){
-				centerLineXStart = xCursorPosition - 50;
+				centerLineXStart = xCursorPosition - xValuePadding;
 			}
-			var centerLineXEnd = xRectRightPosition + 50;
+			var centerLineXEnd = xRectRightPosition + xValuePadding;
 			if(valueList.value > valueList.max){
-				centerLineXEnd = xCursorPosition + 50;
+				centerLineXEnd = xCursorPosition + xValuePadding;
 			}
-			html+=  '   <line x1="'+ centerLineXStart +'" y1="125" x2="'+ centerLineXEnd +'" y2="125" style="stroke:rgb(100,100,100);stroke-width:1;" />'	//central line if data outside the box
+			html+=  '   <line x1="'+ centerLineXStart +'" y1="185" x2="'+ centerLineXEnd +'" y2="185" style="stroke:rgb(100,100,100);stroke-width:1;" />'	//central line if data outside the box
 		}
 		
-		html+=  '   <line x1="'+ xRectLeftPosition +'" y1="125" x2="'+ xRectLeftPosition +'" y2="160" style="stroke:rgb(150,150,150);stroke-width:1;" />'	//left tick
-		html+=  '   <line x1="'+ xRectRightPosition +'" y1="125" x2="'+ xRectRightPosition +'" y2="160" style="stroke:rgb(150,150,150);stroke-width:1;" />'	//right tick
+		html+=  '   <line x1="'+ xRectLeftPosition +'" y1="180" x2="'+ xRectLeftPosition +'" y2="190" style="stroke:rgb(0,0,0);stroke-width:1;" />'	//left tick
+		html+=  '   <line x1="'+ xRectRightPosition +'" y1="180" x2="'+ xRectRightPosition +'" y2="190" style="stroke:rgb(0,0,0);stroke-width:1;" />'	//right tick
 		html+=  '   <line x1="'+ xCursorPosition +'" y1="70" x2="'+ xCursorPosition +'" y2="180" style="stroke:rgb(0,255,0);stroke-width:4;" />'	//cursor for the data
-		html+=  '   <text x="70" y="176" fill="#000000" font-family="calibri" font-size="14">1000 kWh</text>'	//value left
-		html+=  '   <text x="370" y="176" fill="#000000" font-family="calibri" font-size="14">8000 kWh</text>'	//value right
-		html+=  '   <text x="'+ xCursorLabelPosition +'" y="200" fill="#00ff00" font-family="calibri" font-size="16" style="font-weight:bold;">'+ valueList.value +' kWh</text>'	//value data
+		html+=  '   <text x="'+ (xRectLeftPosition - 14) +'" y="206" fill="#000000" font-family="calibri" font-size="14">'+ valueList.min +'</text>'	//value left
+		html+=  '   <text x="'+ (xRectRightPosition - 14) +'" y="206" fill="#000000" font-family="calibri" font-size="14">'+ valueList.max +'</text>'	//value right
+		html+=  '   <text x="'+ xCursorLabelPosition +'" y="60" fill="#00ff00" font-family="calibri" font-size="16" style="font-weight:bold;">'+ valueList.value +' kWh</text>'	//value data
+		html+=	'';
+		
+		var tickIntervalValue = (valueList.max - valueList.min) / (nbTicks-2);
+		for(var i=2; i<nbTicks-1; i++){		//-2: there's already a tick for min and max
+			var xTmpPosition = xRectLeftPosition + ((i*tickIntervalValue - valueList.min)* (rectWidth/(valueList.max - valueList.min)));
+			html+=  '   <line x1="'+ xTmpPosition +'" y1="180" x2="'+ xTmpPosition +'" y2="190" style="stroke:rgb(0,0,0);stroke-width:1;" />'	//tick	
+			html+=  '   <text x="'+ (xTmpPosition - 14) +'" y="206" fill="#000000" font-family="calibri" font-size="14">'+ i*tickIntervalValue +'</text>'	//tick value 
+		}
 		html+= 
 			  '     </svg>'; 
 		divElem.innerHTML = html;
