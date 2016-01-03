6f2fb244895b822f338799a0099fb8bee3a99c90
toni <epardeau.antoine@gmail.com>
2016-01-03 19:56:26 +0100

    modifs

--- js/cubee/chart/CubeeChartFakeBoxPlot.js
+++ js/cubee/chart/CubeeChartFakeBoxPlot.js
@@ -48,9 +48,6 @@ Cubee.CubeeChartFakeBoxPlot = function(divElem, options){
 		html+=  '   <line x1="'+ (centerLineXEnd - 5) +'" y1="182" x2="'+ centerLineXEnd +'" y2="185" style="stroke:rgb(100,100,100);stroke-width:1;" />';	//central line arrow top
 		html+=  '   <line x1="'+ centerLineXEnd +'" y1="185" x2="'+ (centerLineXEnd - 5) +'" y2="188" style="stroke:rgb(100,100,100);stroke-width:1;" />';	//central line arrow bottom
 		html+=  '   <text x="'+ (centerLineXEnd - 5) +'" y="202" fill="#000000" font-family="calibri" font-size="13">kWh</text>';	//unit
-		html+=  '   <line x1="'+ centerLineXStart +'" y1="185" x2="'+ centerLineXEnd +'" y2="185" style="stroke:rgb(0,0,0);stroke-width:1;" />';	//x-axis
-		html+=  '   <line x1="'+ xRectLeftPosition +'" y1="180" x2="'+ xRectLeftPosition +'" y2="190" style="stroke:rgb(0,0,0);stroke-width:1;" />';	//left tick on x-axis
-		html+=  '   <line x1="'+ xRectRightPosition +'" y1="180" x2="'+ xRectRightPosition +'" y2="190" style="stroke:rgb(0,0,0);stroke-width:1;" />';	//right tick on x-axis
 		//html+=  '   <line x1="'+ xCursorPosition(valueList.min) +'" y1="100" x2="'+ xCursorPosition(valueList.min) +'" y2="150" style="stroke:rgb(0,0,0);stroke-width:2;" />';	//cursor for q1
 		html+=  '   <line x1="'+ xCursorPosition(valueList.q1) +'" y1="100" x2="'+ xCursorPosition(valueList.q1) +'" y2="150" style="stroke:rgb(0,0,0);stroke-width:2;" />';	//cursor for q1
 		html+=  '   <line x1="'+ xCursorPosition(valueList.q2) +'" y1="100" x2="'+ xCursorPosition(valueList.q2) +'" y2="150" style="stroke:rgb(0,0,0);stroke-width:2;" />';	//cursor for q2
@@ -61,6 +58,9 @@ Cubee.CubeeChartFakeBoxPlot = function(divElem, options){
 		html+=  '   <text x="'+ (xRectRightPosition - 14) +'" y="206" fill="#000000" font-family="calibri" font-size="14">'+ valueList.max +'</text>';	//value right
 		html+=	'';
 		
+		html+=  '   <line x1="'+ centerLineXStart +'" y1="185" x2="'+ centerLineXEnd +'" y2="185" style="stroke:rgb(0,0,0);stroke-width:1;" />';	//x-axis
+		html+=  '   <line x1="'+ xRectLeftPosition +'" y1="180" x2="'+ xRectLeftPosition +'" y2="190" style="stroke:rgb(0,0,0);stroke-width:1;" />';	//left tick on x-axis
+		html+=  '   <line x1="'+ xRectRightPosition +'" y1="180" x2="'+ xRectRightPosition +'" y2="190" style="stroke:rgb(0,0,0);stroke-width:1;" />';	//right tick on x-axis
 		var tickIntervalValue = (valueList.max - valueList.min) / (nbTicks-2);
 		for(var i=2; i<nbTicks-1; i++){		//-2: there's already a tick for min and max
 			var xTmpPosition = xRectLeftPosition + ((i*tickIntervalValue - valueList.min)* (rectWidth/(valueList.max - valueList.min)));
